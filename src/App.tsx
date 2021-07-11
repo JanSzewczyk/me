97bad8e4c3359bf1656c1111f371a92804cea358
JanSzewczyk <jan.szewczyk1997@gmail.com>
2021-07-11 21:34:25 +0200

    Add travis config

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Jan Robert Szewczyk
+          <br/>
+          The Best Web Developer
         </p>
         <a
           className="App-link"
